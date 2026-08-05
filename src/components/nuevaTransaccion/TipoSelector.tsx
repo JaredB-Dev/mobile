9014cb053fd8213e57e198ed57d05f0016f069d1
jossanchcald <jossanch@espol.edu.ec>
2026-08-05 04:09:11 -0500

    Cambio de colores por baja accesibilidad

--- src/components/nuevaTransaccion/TipoSelector.tsx
+++ src/components/nuevaTransaccion/TipoSelector.tsx
@@ -9,32 +9,11 @@ interface TipoSelectorProps {
 const TipoSelector: React.FC<TipoSelectorProps> = ({ tipo, onTipoChange }) => {
   return (
     <IonSegment value={tipo} onIonChange={(e) => onTipoChange(e.detail.value as 'ingreso' | 'gasto')}>
-      <IonSegmentButton
-        value="gasto"
-        style={{
-          '--background': 'var(--ion-color-danger-tint)',
-          '--background-checked': 'var(--ion-color-danger-tint)',
-          '--color': 'var(--ion-color-danger-shade)',
-          '--color-checked': 'var(--ion-color-danger-shade)',
-          '--indicator-color': 'transparent',
-          opacity: tipo === 'gasto' ? 1 : 0.55,
-        } as React.CSSProperties}
-      >
+      <IonSegmentButton value="gasto">
         <IonIcon icon={cardOutline} />
         <IonLabel>Gasto</IonLabel>
       </IonSegmentButton>
-
-      <IonSegmentButton
-        value="ingreso"
-        style={{
-          '--background': 'var(--ion-color-success-tint)',
-          '--background-checked': 'var(--ion-color-success-tint)',
-          '--color': 'var(--ion-color-success-shade)',
-          '--color-checked': 'var(--ion-color-success-shade)',
-          '--indicator-color': 'transparent',
-          opacity: tipo === 'ingreso' ? 1 : 0.55,
-        } as React.CSSProperties}
-      >
+      <IonSegmentButton value="ingreso">
         <IonIcon icon={cashOutline} />
         <IonLabel>Ingreso</IonLabel>
       </IonSegmentButton>
